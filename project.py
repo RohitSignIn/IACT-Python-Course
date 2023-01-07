a479eca9b9c00299d98cec0660a9b6679e172f4a
Rohit <rohitsinghsignin@gmail.com>
2023-01-07 15:54:49 +0530

    New

---
+++ project.py
@@ -0,0 +1,23 @@
+Questions = [
+    { 'question': 'What does the abbreviation HTML stand for', 
+        'a': 'HyperText Markup Language', 
+        'b': 'Hyper Markup Language',
+        'c': 'HyperText M Language', 
+        'd': 'HText Markup Language',
+        'ans': 'a' },
+        { 'question': 'The correct sequence of HTML tags for starting a webpage is -', 
+        'a': 'Head, Title, HTML, body', 
+        'b': 'HTML, Body, Title, Head',
+        'c': 'HTML, Head, Title, Body', 
+        'd': 'HTML, Head, Title, Body',
+        'ans': 'd' },
+        { 'question': 'Which of the following element is responsible for making the text bold in HTML?', 
+        'a': '<pre>', 
+        'b': '<a>',
+        'c': '<b>', 
+        'd': '<br>',
+        'ans': 'c' }
+]
+
+for val in Questions:
+    print(val["question"])
